1534a939ea1ff60b19b9049cf9c3e21ddaa1e83a
Alexandra <kust997@mail.ru>
2023-06-16 17:22:45 +0300

    add another page + minifed images

--- js/script.js
+++ js/script.js
@@ -11,10 +11,8 @@ phoneChanges.forEach(function (phoneChange) {
 	var result = Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7);
 	if (result % 2 === 1) {
 		phoneFirst.classList.add('d-none');
-		if (phoneLast.classList.contains('d-none')) phoneLast.classList.remove('d-none');
 	} else {
 		phoneLast.classList.add('d-none');
-		if (phoneFirst.classList.contains('d-none')) phoneFirst.classList.remove('d-none');
 	}
 })
 
@@ -45,6 +43,7 @@ window.addEventListener('resize', function () {
 
 let burger = document.querySelector('.header__burger');
 burger.addEventListener('click', function () {
+	burger.classList.toggle('burger--active');
 	nav.classList.toggle('nav--active');
 	header.classList.toggle('header--active');
 	document.body.classList.toggle('lock');
@@ -59,6 +58,7 @@ burger.addEventListener('click', function () {
 })
 
 function closeMenu() {
+	burger.classList.remove('burger--active');
 	nav.classList.remove('nav--active');
 	document.body.classList.remove('lock');
 }
@@ -228,6 +228,9 @@ $('[data-download-form-js]').each(function () {
 			phone: {
 				required: true,
 				checkMaskPhone: true
+			},
+			email: {
+				required: true
 			}
 		},
 		errorPlacement: function (error, element) { },
@@ -370,8 +373,16 @@ quizSingle.forEach(function (item) {
 
 // progressbar
 function progress(num) {
+	let quizText = [
+		"Пояснение от менеджера по 1 вопросу, Пояснение от менеджера по 1 вопросу",
+		"Пояснение от менеджера по 2 вопросу, Пояснение от менеджера по 2 вопросу",
+		"Пояснение от менеджера по 3 вопросу, Пояснение от менеджера по 3 вопросу",
+		"Пояснение от менеджера по 4 вопросу, Пояснение от менеджера по 4 вопросу",
+		"Пояснение от менеджера по 5 вопросу, Пояснение от менеджера по 5 вопросу",
+	];
 	const percent = parseInt((100 / maxNumber) * (num + 1));
 	$('.js-quiz').text(num + 1);
+	$('.quiz-plate__text').html(quizText[num]);
 	$('.quiz__progress-inner').css('width', (percent === 100 ? 98.5 : percent) + '%');
 }
 progress(0);
@@ -467,7 +478,7 @@ function inputValid(item) {
 }
 
 let quiz4Inputs = document.querySelectorAll('input[name="quiz4"]');
-if (quiz4Inputs) {
+if (quiz4Inputs.length > 0) {
 	inputWidth = document.getElementById('quiz4-1');
 	inputLenght = document.getElementById('quiz4-2');
 	inputSkip = document.getElementById('quiz4-3');
@@ -486,6 +497,49 @@ if (quiz4Inputs) {
 			}
 		};
 	})
+}
+
+
+const valuesOfWidth = [0, 200, 300, 400, 600, 800, 900, 1000];
+const sliderWidth = document.getElementById('quiz4-1');
+let sliderRowOfNumbers = document.querySelectorAll('.quiz-option__range');
+if (sliderRowOfNumbers.length > 0 && sliderWidth) {
+	valuesOfWidth.forEach(function (item) {
+		let p = document.createElement('p');
+		p.className = "quiz-option__number";
+		p.style.left = (100 / valuesOfWidth.length) * valuesOfWidth.indexOf(item) + "%";
+		p.innerHTML = item;
+		sliderRowOfNumbers[0].append(p);
+	})
+
+	sliderWidth.max = valuesOfWidth.length - 1;
+	sliderWidth.oninput = function () {
+		// console.log(valuesOfWidth[this.value]);
+	};
+	sliderWidth.oninput();
+}
+
+
+const valuesOfLenght = [0, 1000, 1500, 2000, 3000, 3500, 4000, 4500];
+const sliderLenght = document.getElementById('quiz4-2');
+if (sliderRowOfNumbers.length > 0 && sliderLenght) {
+	valuesOfLenght.forEach(function (item) {
+		let p = document.createElement('p');
+		p.className = "quiz-option__number";
+		p.style.left = (100 / valuesOfLenght.length) * valuesOfLenght.indexOf(item) + "%";
+		p.innerHTML = item;
+
+		if (!valuesOfLenght.indexOf(item) == 0 || !valuesOfLenght.indexOf(item) == valuesOfLenght.length - 1) {
+			p.style.marginLeft = "-0.6rem";
+		}
+		sliderRowOfNumbers[1].append(p);
+	})
+
+	sliderLenght.max = valuesOfLenght.length - 1;
+	sliderLenght.oninput = function () {
+		// console.log(valuesOfLenght[this.value]);
+	};
+	sliderLenght.oninput();
 }
 
 
@@ -554,8 +608,11 @@ function swiperWarranty() {
 		init = false;
 	}
 }
-swiperWarranty();
-window.addEventListener("resize", swiperWarranty);
+
+if (document.querySelector('.warranty__swiper')) {
+	swiperWarranty();
+	window.addEventListener("resize", swiperWarranty);
+}
 
 
 // анимация подгрузки контента при скролле
@@ -683,44 +740,6 @@ if (hideTheModal == null) {
 	}, 3000);
 }
 
-const valuesOfWidth = [0, 200, 300, 400, 600, 800, 900, 1000];
-const sliderWidth = document.getElementById('quiz4-1');
-let sliderRowOfNumbers = document.querySelectorAll('.quiz-option__range');
-valuesOfWidth.forEach(function (item) {
-	let p = document.createElement('p');
-	p.className = "quiz-option__number";
-	p.style.left = (100 / valuesOfWidth.length) * valuesOfWidth.indexOf(item) + "%";
-	p.innerHTML = item;
-	sliderRowOfNumbers[0].append(p);
-})
-
-sliderWidth.max = valuesOfWidth.length - 1;
-sliderWidth.oninput = function () {
-	// console.log(valuesOfWidth[this.value]);
-};
-sliderWidth.oninput();
-
-const valuesOfLenght = [0, 1000, 1500, 2000, 3000, 3500, 4000, 4500];
-const sliderLenght = document.getElementById('quiz4-2');
-valuesOfLenght.forEach(function (item) {
-	let p = document.createElement('p');
-	p.className = "quiz-option__number";
-	p.style.left = (100 / valuesOfLenght.length) * valuesOfLenght.indexOf(item) + "%";
-	p.innerHTML = item;
-
-	if (!valuesOfLenght.indexOf(item) == 0 || !valuesOfLenght.indexOf(item) == valuesOfLenght.length - 1) {
-		p.style.marginLeft = "-0.6rem";
-	}
-	sliderRowOfNumbers[1].append(p);
-
-})
-
-sliderLenght.max = valuesOfLenght.length - 1;
-sliderLenght.oninput = function () {
-	// console.log(valuesOfLenght[this.value]);
-};
-sliderLenght.oninput();
-
 
 const sliderEl = document.querySelectorAll("input[type='range']");
 sliderEl.forEach(function (range) {
@@ -734,3 +753,47 @@ sliderEl.forEach(function (range) {
 		range.style.background = `linear-gradient(to right, #42B214 ${progress}%, #cacaca ${progress}%)`;
 	})
 })
+
+// липкое боковое меню
+let sideMenu = document.querySelector('.sidemenu');
+if (sideMenu) {
+	let sideClose = sideMenu.querySelector('.sidemenu__close');
+	let sideBody = sideMenu.querySelector('.sidemenu__body');
+	let sideTop = sideMenu.querySelector('.sidemenu__top');
+	sideBody.style.maxHeight = sideBody.scrollHeight / 10 + "rem";
+
+	window.addEventListener('resize', function () {
+		sideBody.style.maxHeight = sideBody.scrollHeight / 10 + "rem";
+	})
+
+	sideClose.addEventListener('click', function () {
+		if (sideBody.style.maxHeight == null && !sideTop.classList.contains('active')) {
+			sideBody.style.maxHeight = sideBody.scrollHeight / 10 + "rem";
+		} else {
+			sideBody.style.maxHeight = null;
+			sideMenu.classList.add('hidden');
+		}
+
+		setTimeout(function () {
+			sideClose.classList.add('close');
+			sideTop.classList.add('close');
+		}, 1200)
+
+		if (sideMenu.classList.contains('hidden')) {
+			setTimeout(function () {
+				sideClose.classList.remove('close');
+				sideTop.classList.remove('close');
+				sideMenu.classList.remove('hidden');
+			}, 15000)
+		}
+	})
+
+	sideTop.addEventListener('click', function () {
+		sideTop.classList.toggle('active');
+		if (sideBody.style.maxHeight) {
+			sideBody.style.maxHeight = null;
+		} else {
+			sideBody.style.maxHeight = sideBody.scrollHeight / 10 + "rem";
+		}
+	})
+}
--- mailer/send.php
+++ mailer/send.php
@@ -12,14 +12,14 @@ $answer_1 = $_POST["quiz1"];
 $question_2 = $_POST['title-2'];
 $answer_2 = $_POST['quiz2'];
 
-$question_2_2 = $_POST['title-2_2'];
-$answer_2_2 = $_POST['quiz2_2'];
-
 $question_3 = $_POST['title-3'];
 $answer_3 = $_POST['quiz3'];
 
-$question_4 = $_POST['title-4'];
+$question_4 = $_POST['title-4-1'];
 $answer_4 = $_POST['quiz4'];
+
+$question_4_2 = $_POST['title-4-2'];
+$answer_4_2 = $_POST['quiz4'];
 
 $question_5 = $_POST['title-5'];
 $answer_5 = $_POST['quiz5'];
@@ -33,9 +33,15 @@ $msg = '
 Пользователь заказал обратный звонок. <br>
 Телефон: <b>' . $phone .' </b><br>
 ';
-} else if ($_POST['formname'] == 'how') {
+} else if ($_POST['formname'] == 'catalog') {
 $msg = '
-Пользователь скачал каталог "Топ-7 советов как выбрать систему отопления раз и навсегда" <br>
+Пользователь скачал каталог "Как правильно выбрать мебельный щит для вашего проекта и сэкономить" <br>
+Телефон: <b> ' . $phone .' </b><br>
+Способ связи: <b> ' . $social . ' </b>
+';
+} else if ($_POST['formname'] == 'urface') {
+$msg = '
+Пользователь скачал каталог для <b>юр. лиц</b><br>
 Телефон: <b> ' . $phone .' </b><br>
 Способ связи: <b> ' . $social . ' </b>
 ';
@@ -43,15 +49,15 @@ $msg = '
 $msg = '
 Пользователь прошёл тест: <br>
 1. ' . $question_1 . ' Ответ: <b>' . $answer_1 . ' </b><br>
-2.1. ' . $question_2 . ' Ответ: <b>' . $answer_2 . ' </b><br>
-2.2. ' . $question_2_2 . ' Ответ: <b>' . $answer_2_2 . ' </b><br>
+2. ' . $question_2 . ' Ответ: <b>' . $answer_2 . ' </b><br>
 3. ' . $question_3 . ' Ответ: <b>' . $answer_3 . ' </b><br>
-4. ' . $question_4 . ' Ответ: <b>' . $answer_4 . ' </b><br>
+4.1. ' . $question_4 . ' Ответ: <b>' . $answer_4 . ' </b><br>
+4.2. ' . $question_4_2 . ' Ответ: <b>' . $answer_4_2 . ' </b><br>
 5. ' . $question_5 . ' Ответ: <b>' . $answer_5 . ' </b><br>
-6. ' . $question_6 . ' Ответ: <b>' . $answer_6 . ' </b><br>
 <br>
-Телефон: <b> ' . $phone .' </b><br>
-Способ связи: <b> ' . $social . ' </b>
+' . $question_6 . ' Ответ: <b>' . $answer_6 . ' </b><br>
+<br>
+Контакт: <b> ' . $phone .' ' . $email .' </b><br>
 ';
 }
 
